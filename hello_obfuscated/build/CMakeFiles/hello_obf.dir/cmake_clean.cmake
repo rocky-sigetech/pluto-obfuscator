9d69f621e4dc6feb669b3c4008097237646fb737
rocky-sigetech <117636788+rocky-sigetech@users.noreply.github.com>
2024-06-25 17:43:21 +0530

    obfuscation test code

---
+++ hello_obfuscated/build/CMakeFiles/hello_obf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_obf.dir/hello.c.o"
+  "hello_obf"
+  "hello_obf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_obf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
